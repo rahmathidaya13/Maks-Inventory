74612bd80d0e27d906fa86cba5ec8b1c6bc63067
rahmathidaya13 <rahmathidaya1305@gmail.com>
2024-11-21 12:13:37 +0700

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        obfuscator(),
     ],
     build: {
         sourcemap: false, // Jangan menghasilkan sourcemap
