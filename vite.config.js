53478f32ccbb0e7667f1e6f3c8d568e627788a48
rahmathidaya13 <rahmathidaya1305@gmail.com>
2024-11-21 12:15:27 +0700

    edit vite rollback

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
-        obfuscator(),
     ],
     build: {
         sourcemap: false, // Jangan menghasilkan sourcemap
